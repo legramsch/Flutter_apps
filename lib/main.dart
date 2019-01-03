664e9519a1fd9e62a2112e2c23031ad6b5c96fd1
legramsch <legramsch@uc.cl>
2019-01-03 15:55:37 -0300

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -3,55 +3,69 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mindless Test',
+      title: 'Startup Name Generator',
       home: RandomWords(),
     );
   }
+// #enddocregion build
 }
+// #enddocregion MyApp
 
+// #docregion RWS-var
 class RandomWordsState extends State<RandomWords> {
-  final _suggestion = <WordPair>[];
+  final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold (
-    appBar: AppBar(
-      title: Text('Prueba name generator'),
-    ),
-    body: _buildSuggestions(),
-  );
-  }
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
-          final index = i ~/ 2;
-          if (index >= _suggestion.length) {
-            _suggestion.addAll(generateWordPairs().take(10));
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-          return _buildRow(_suggestion[index]);
-        }
-    );
+          return _buildRow(_suggestions[index]);
+        });
   }
+  // #enddocregion _buildSuggestions
 
+  // #docregion _buildRow
   Widget _buildRow(WordPair pair) {
     return ListTile(
-        title: Text (
-          pair.asPascalCase,
-          style: _biggerFont,
-        )
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
     );
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+// #enddocregion RWS-build
+// #docregion RWS-var
 }
+// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
-
-
-  }
-
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
